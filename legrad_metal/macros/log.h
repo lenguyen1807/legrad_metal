d55f7af2a3a16cc768c96f1d156b40ae4d05e883
Le Nguyen <lenguyen18072003@gmail.com>
2025-04-15 21:18:56 +0700

    feat: change log to fmt instead of boost

--- legrad_metal/macros/log.h
+++ legrad_metal/macros/log.h
@@ -19,31 +19,39 @@
 namespace logging = boost::log;
 namespace keywords = boost::log::keywords;
 
-/*
- * Because I don't want to make the logging system too complicated
- * So we will relied on Boost for now
- */
-LEGRAD_INLINE void init_console_logging()
+// https://fmt.dev/11.1/api/#compile-time-checks
+LEGRAD_INLINE void vlog(const char* type,
+                        const char* file,
+                        int line,
+                        fmt::string_view fmt,
+                        fmt::format_args args)
 {
-  logging::add_console_log(
-      std::cout, keywords::format = "[%TimeStamp%][%Severity%]%Message%");
-  logging::add_common_attributes();
+  fmt::print("[{}] ({}, {}): {}\n", type, file, line, fmt::vformat(fmt, args));
 }
 
-LEGRAD_INLINE void init_file_logging()
+template <typename... T>
+void log(const char* type,
+         const char* file,
+         int line,
+         fmt::format_string<T...> fmt,
+         T&&... args)
 {
-  logging::add_file_log(
-      keywords::file_name = "tensor_library.log",
-      keywords::format = "[%TimeStamp%][%Severity%]: %Message%",
-      keywords::auto_flush = true);
-  logging::add_common_attributes();
+  vlog(type, file, line, fmt, fmt::make_format_args(args...));
 }
 
+#ifdef USE_BOOST
 #define LOG_INFO_STREAM BOOST_LOG_TRIVIAL(info)
 #define LOG_DEBUG_STREAM BOOST_LOG_TRIVIAL(debug)
 #define LOG_TRACE_STREAM BOOST_LOG_TRIVIAL(trace)
 #define LOG_ERROR_STREAM BOOST_LOG_TRIVIAL(error)
 #define LOG_WARN_STREAM BOOST_LOG_TRIVIAL(warning)
+#else
+#define LOG_INFO_STREAM "info"
+#define LOG_DEBUG_STREAM "debug"
+#define LOG_TRACE_STREAM "trace"
+#define LOG_ERROR_STREAM "error"
+#define LOG_WARN_STREAM "warning"
+#endif
 
 // clang-format off
 /*
@@ -60,10 +68,14 @@ LEGRAD_INLINE void init_file_logging()
 #define LEGRAD_FILENAME \
   (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 
+#ifdef USE_BOOST
 #define LEGRAD_LOG_TEMPLATE(logger, msg, ...) \
   logger << "(" << __PRETTY_FUNCTION__ << ", " << LEGRAD_FILENAME << ", " \
          << static_cast<uint32_t>(__LINE__) \
          << "): " << LEGRAD_FORMAT_STR(msg, __VA_ARGS__)
+#else
+#define LEGRAD_LOG_TEMPLATE(logger, msg, ...) log(logger, __PRETTY_FUNCTION__, __LINE__, msg, __VA_ARGS__)
+#endif
 
 #define LEGRAD_LOG_INFO(msg, ...) LEGRAD_LOG_TEMPLATE(LOG_INFO_STREAM, msg, __VA_ARGS__);
 #define LEGRAD_LOG_DEBUG(msg, ...) LEGRAD_LOG_TEMPLATE(LOG_DEBUG_STREAM, msg, __VA_ARGS__);
