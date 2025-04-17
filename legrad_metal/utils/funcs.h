98839fa0501d96056e1fdcfbfd0da66e0bb6c3ec
Le Nguyen <lenguyen18072003@gmail.com>
2025-04-17 17:02:24 +0700

    add wrap_dim function

--- legrad_metal/utils/funcs.h
+++ legrad_metal/utils/funcs.h
@@ -7,6 +7,7 @@
 
 #include "boost/functional/hash.hpp"
 #include "macros/expr.h"
+#include "macros/log.h"
 
 namespace utils
 {
@@ -57,4 +58,37 @@ struct HashPairKey
     return seed;
   }
 };
+
+// Taken from Pytorch
+template <typename T>
+LEGRAD_INLINE T _wrap_dim(T dim, T rank)
+{
+  if (LEGRAD_LIKELY(rank * T{-1} <= dim && dim < rank)) {
+    if (dim <= 0) {
+      return dim + rank;
+    }
+    return dim;
+  } else {  // If cannot hit the first case, should throw error
+    LEGRAD_CHECK_AND_THROW(rank >= 0, std::runtime_error,
+                           "Rank cannot be negative", 0);
+
+    LEGRAD_CHECK_AND_THROW(
+        rank != 0, std::runtime_error,
+        "Dimension {} is specified but the Tensor is empty (has 0 rank)", dim);
+
+    T min = rank * T{-1};
+    T max = rank - T{1};
+
+    LEGRAD_CHECK_AND_THROW(min <= dim && dim <= max, std::runtime_error,
+                           "Dimension {} is out of range [{}, {}]", dim, min,
+                           max);
+
+    LEGRAD_THROW_ERROR(std::logic_error, "All path above should been hit", 0);
+  }
+}
+
+LEGRAD_INLINE int64_t maybe_wrap_dim(int64_t dim, int64_t rank)
+{
+  return _wrap_dim(dim, rank);
+}
 }  // namespace utils
